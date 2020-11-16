fab9f844baa2310359c2c6687dd9458b0b03e14d
ValentinRouge <rougevalentin@outlook.com>
2020-11-16 19:15:50 +0100

    auto pilot

--- auto_pilot.py
+++ auto_pilot.py
@@ -1,2 +1,16 @@
 from intersection_2_droites.intersection2droites import *
+
+LCmin = input("latitude minimale de la carte : ")
+LCmax = input("latitude maximale de la carte : ")
+lCmin = input("longitude minimale de la carte : ")
+lCmax = input("longitude maximale de la carte : ")
+
 A1 = input("angle de vision du premier phare : ")
+L1 = input("latitude du premier phare : ")
+l1 = input("longitude du premier phare : ")
+
+A2 = input("angle de vision du second phare : ")
+L2 = input("latitude du second phare : ")
+l2 = input("longitude du second phare : ")
+
+L1 = 
