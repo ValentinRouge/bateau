b0efe3d3f374b60690e6dd4a01b31638b56ba7f7
jb <jb.moufle@gmail.com>
2020-11-23 18:43:40 +0100

    phares

--- export_list.py
+++ export_list.py
@@ -9,5 +9,5 @@ for i in range(len(data_dict['features'])):
         if list(data_dict['features'][i]['properties'].keys()).__contains__('name'):
             phares[data_dict['features'][i]['properties']['name'].replace('Ã©', 'e').replace('Ã¼', 'u').replace('Ã«', 'e').replace('ÃŽ', 'I').replace('Ãª', 'e').replace('Ã‰', 'E').replace('Ã´', 'o').replace('Ã®', 'I').replace('Ã¨', 'e').replace('Ã ', 'a').replace('Ã¢', 'a').lower()] = (data_dict['features'][i]['geometry']['coordinates'][0], data_dict['features'][i]['geometry']['coordinates'][1])
 
-"""for i in range(len(list(phares.keys()))):
-    print(list(phares.keys())[i], phares[list(phares.keys())[i]])"""
+for i in range(len(list(phares.keys()))):
+    print(list(phares.keys())[i], phares[list(phares.keys())[i]])
