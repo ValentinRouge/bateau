3ef869bfac8e163bb82126df39b4afca810b7183
jb <jb.moufle@gmail.com>
2021-01-04 16:08:20 +0100

    modifs nombreuses calcul de pos

--- auto_pilot.py
+++ auto_pilot.py
@@ -4,6 +4,11 @@ from maree.maree import *
 from maree.func_maree import *
 from convertisseur_de_coordonnees.coordonnee import *
 from cascade_resultat.cascade_resultats import *
+from json import *
+from math import sqrt
+
+with open('phare_de_France_dict.json') as json_data:  # on charge la liste des phare de france
+    phare_de_france = load(json_data)
 
 while True:
     print("""
@@ -22,42 +27,74 @@ while True:
     if answ == 0:
         break
     elif answ == 1:
-        # a entrer a partir du moment où il y a des données en lat / long qui sont rentrée
-        try :
-            LCmin==0
-        except : 
-            print("-" * 40, sep="")
-            print("    Carte:")
-            LCmin = input("\t-latitude minimale : ")   # Pour l'émisphére Nord seulement et sans prendre en compte le passage de l'antiméridien
-            LCmax = input("\t-latitude maximale : ")
-            lCmin = input("\n\t-longitude minimale : ")
-            lCmax = input("\t-longitude maximale : ")
-            print("-" * 40, sep="")
+        while True:
+            try:
+                # a entrer a partir du moment où il y a des données en lat / long qui sont rentrée
+                try :
+                    if LCmin!=300:
+                        pass
+                    else:
+                        print("-" * 40, sep="")
+                        print("    Carte:")
+                        LCmin = input("\t-latitude minimale : ")   # Pour l'émisphére Nord seulement et sans prendre en compte le passage de l'antiméridien
+                        LCmax = input("\t-latitude maximale : ")
+                        lCmin = input("\n\t-longitude minimale : ")
+                        lCmax = input("\t-longitude maximale : ")
+                    print("-" * 40, sep="")
+                except NameError: 
+                    print("-" * 40, sep="")
+                    print("    Carte:")
+                    LCmin = input("\t-latitude minimale : ")   # Pour l'émisphére Nord seulement et sans prendre en compte le passage de l'antiméridien
+                    LCmax = input("\t-latitude maximale : ")
+                    lCmin = input("\n\t-longitude minimale : ")
+                    lCmax = input("\t-longitude maximale : ")
+                    print("-" * 40, sep="")
+                LCmin = float(LCmin)
+                LCmax = float(LCmax)
+                lCmin = float(lCmin)
+                lCmax = float(lCmax)
 
-        print("")
-        print("Phare 1")
-        choice = input(":")
-        if 0 < choic < 10:
-            pass
-        elif choice == 0:
-            break
-        # a rentrer pour se localiser sur la carte (droite + intersection)
-        else:
-            print("-" * 40, sep="")
-            print("    Phare 1:")
-            A1 = input("\t-angle de vision (°): ")
-            L1 = input("\n\t-latitude : ")
-            l1 = input("\t-longitude : ")
-            print("-" * 40, sep="")
+                print("")
+                distance_phare = []
+                for phare in list(phare_de_france.keys()):
+                    distance_phare.append(sqrt((abs(phare_de_france[phare][0] - abs(LCmax-LCmin)))*(abs(phare_de_france[phare][0] - abs(LCmax-LCmin))) + (abs(phare_de_france[phare][1] - abs(lCmax-lCmin)))*(abs(phare_de_france[phare][1] - abs(lCmax-lCmin)))))
+                #print(sorted(distance_phare))
+                print("Phare 1")
+                choice = input(":")
+                if 0 < choic < 10:
+                    pass
+                elif choice == 0:
+                    break
+                # a rentrer pour se localiser sur la carte (droite + intersection)
+                else:
+                    print("-" * 40, sep="")
+                    print("    Phare 1:")
+                    A1 = input("\t-angle de vision (°): ")
+                    L1 = input("\n\t-latitude : ")
+                    l1 = input("\t-longitude : ")
+                    print("-" * 40, sep="")
 
-            print("")
-            print("-" * 40, sep="")
-            print("    Phare 2:")
-            A2 = input("\t-angle de vision (°): ")
-            L2 = input("\n\t-latitude : ")
-            l2 = input("\t-longitude : ")
-            print("-" * 40, sep="")
+                    print("")
+                    print("-" * 40, sep="")
+                    print("    Phare 2:")
+                    A2 = input("\t-angle de vision (°): ")
+                    L2 = input("\n\t-latitude : ")
+                    l2 = input("\t-longitude : ")
+                    print("-" * 40, sep="")
+                    if A1 == A2 or A1 == A2+180 or A1+180 == A2:
+                        print("\nimpossible de rentrer 2 angles équivalent à 180° prés\n")
+                    else:
+                        break
 
+            except ValueError:
+                print("""
+Il faut rentrer les latitudes/longitude sous le format: hh.mmss,
+et les angles de vision sous la forme: ddd (entre 0 et 360)
+""")
+                LCmin = 300
+                LCmax = 0
+                lCmin = 0
+                lCmax = 0
 
         X1,Y1 = convertDepuisLatLong(LCmax, LCmin, lCmax, lCmin, L1, l1)
         X2,Y2 = convertDepuisLatLong(LCmax, LCmin, lCmax, lCmin, L2, l2)
